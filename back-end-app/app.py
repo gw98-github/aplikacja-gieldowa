fb8071a2c26dbb9e8b8a60b53b02f9f1aec810bc
pwatorski <piotr.watorski@gmail.com>
2021-11-04 21:44:43 +0100

    data generation

--- back-end-app/app.py
+++ back-end-app/app.py
@@ -3,14 +3,19 @@ from flask_restful import Api, Resource, reqparse
 import sqlalchemy
 #from flask_cors import CORS #comment this on deployment
 from data_request_handler import DataRequestHandler
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sarna@localhost/sarna'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:sarna@localhost:5432/sarna'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'secret string'
 
-db = sqlalchemy(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
 
 #CORS(app) #comment this on deployment
 api = Api(app)
@@ -20,6 +25,37 @@ def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
 api.add_resource(DataRequestHandler, '/flask/data')
+
+class Apisource(db.Model):
+    __tablename__ = 'api_source'
+    id = db.Column(db.Integer, primary_key=True)
+    api_name = db.Column(db.String(80), unique=True, nullable=False)
+    last_checked = db.Column(db.DateTime, nullable=False)
+
+
+class Stock (db.Model):
+    __tablename__ = 'stock'
+    id = db.Column(db.Integer, primary_key=True)
+    stock_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_checked = db.Column(db.DateTime, nullable=False)
+    api_id = db.Column(db.Integer, db.ForeignKey('api_source.id'))
+
+
+class Company (db.Model):
+    __tablename__ = 'company'
+    id = db.Column(db.Integer, primary_key=True)
+    company_name = db.Column(db.String(80), unique=False, nullable=False)
+    stock_id = db.Column(db.Integer, db.ForeignKey('stock.id'))
+
+
+class Action (db.Model):
+    __tablename__ = 'action'
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Float, unique=False, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
+
+
 
 if __name__ == '__main__':
     app.run()
--- back-end-app/data_request_handler.py
+++ back-end-app/data_request_handler.py
@@ -2,14 +2,24 @@ from flask_restful import Api, Resource, reqparse
 from datetime import datetime, timedelta
 import random
 
+def dane_z_nikad():
+  td = timedelta(hours=1)
+  begin = datetime.now()-timedelta(hours=100)
+  data = {}
+  value = 400
+  for e in range(100):
+      value = max(100, value + int((random.random() * 2 - 1)**7 * 100))
+      data[begin.strftime('%d-%m-%Y %H:00')] = value
+      begin += td
+  
+  final_ret = {"data":data}
+
+  return final_ret
 
 class DataRequestHandler(Resource):
 
   def get(self):
-    return {
-      'resultStatus': 'SUCCESS',
-      'message': "Hello Api Handler"
-      }
+    return dane_z_nikad()
 
 
   def post(self):
@@ -30,12 +40,12 @@ class DataRequestHandler(Resource):
     ret_status = request_type
     ret_msg = request_json
     td = timedelta(hours='100')
-    begin = datetime.now().strftime('%m-%d-%Y;%H-%M-%S')
+    begin = (datetime.now()-td).strftime('%m-%d-%Y;%H-%M-%S')
     density = 'hour'
     data = [400 for x in range(100)]
     for e, d in enumerate(data[1:]):
         data[e+1] =  max(100, data[e] + int((random.random() * 10)**2))
     
-    final_ret = {"status": "Success", "begin": begin, "density": density, "data":data}
+    final_ret = {"data":data}
 
     return final_ret
