db03af076704450080d8ed4407c741a4b2e68717
pwatorski <piotr.watorski@gmail.com>
2021-11-11 13:02:35 +0100

    improve db populating script

--- back-end-app/populate_db.py
+++ back-end-app/populate_db.py
@@ -4,11 +4,29 @@ from models import Apisource, Stock, Company, Action
 from misc import dane_z_nikad
 
 def clear_db(db):
-    db.session.query(Action).delete()
-    db.session.query(Company).delete()
-    db.session.query(Stock).delete()
-    db.session.query(Apisource).delete()
-    db.session.commit()
+    print('Removing Action, Company, Stock, Apisource tables...')
+    try:
+        db.session.query(Action).delete()
+    except:
+        pass
+    try:
+        db.session.query(Company).delete()
+    except:
+        pass
+    try:
+        db.session.query(Stock).delete()
+    except:
+        pass
+    try:
+        db.session.query(Apisource).delete()
+    except:
+        pass
+    print('Creating Action, Company, Stock, Apisource tables...')
+    db.create_all()
+    try:
+        db.session.commit()
+    except:
+        pass
 
 def create_apisource(db, name:str):
     apisource = Apisource(name)
@@ -42,10 +60,12 @@ def populate(db, companies:List[str]=['CDP', 'Tesla', 'Game Stop']):
     print('Creating stock...')
     stock_id = create_stock(db, 'Some Stock', api_id)
     print('Creating companies:')
+    steps = 1000
     for company in companies:
         print(f'\tCreating {company}...')
         company_id = create_company(db, company, stock_id)
-        create_actions(db, company_id)
+        create_actions(db, company_id, steps=steps)
+        print(f'\t\tInserted {steps} Action records.')
     print('Finished :)')
 
 populate(db)
