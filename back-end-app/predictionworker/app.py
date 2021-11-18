a320c80379bfdd5c8235fd41f5bb8b2ef2bfc463
pwatorski <piotr.watorski@gmail.com>
2021-11-19 00:35:40 +0100

    test

--- back-end-app/predictionworker/app.py
+++ back-end-app/predictionworker/app.py
@@ -2,9 +2,6 @@ import pika
 import psycopg2
 
 
-
-
-
 def callback(ch, method, properties, body):
     print(" [x] Received %s" % body)
     symbol = body.decode()
@@ -14,12 +11,12 @@ def callback(ch, method, properties, body):
 
 
 
-credentials = pika.PlainCredentials('sarna', 'sarna')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', credentials=credentials))
-db_conn = psycopg2.connect(database='sarna', user='postgres', host='localhost', password='sarna')
+#credentials = pika.PlainCredentials('sarna', 'sarna')
+#connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', credentials=credentials))
+db_conn = psycopg2.connect(database='sarna', user='postgres', host='0.0.0.0', password='sarna')
 cur = db_conn.cursor()
-channel = connection.channel()
-channel.queue_declare(queue='predict_queue', durable=True)
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='predict_queue', on_message_callback=callback)
-channel.start_consuming()
+#channel = connection.channel()
+#channel.queue_declare(queue='predict_queue', durable=True)
+#channel.basic_qos(prefetch_count=1)
+#channel.basic_consume(queue='predict_queue', on_message_callback=callback)
+#channel.start_consuming()
--- back-end-app/stockapiworker/app.py
+++ back-end-app/stockapiworker/app.py
@@ -19,17 +19,16 @@ def callback(ch, method, properties, body):
     print(" [x] Received %s" % body)
     symbol = body.decode()
     
-    
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 
-credentials = pika.PlainCredentials('sarna', 'sarna')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', credentials=credentials))
+#credentials = pika.PlainCredentials('sarna', 'sarna')
+#connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', credentials=credentials))
 db_conn = psycopg2.connect(database='sarna', user='postgres', host='0.0.0.0', password='sarna')
 cur = db_conn.cursor()
-channel = connection.channel()
-channel.queue_declare(queue='task_queue', durable=True)
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue='task_queue', on_message_callback=callback)
-channel.start_consuming()
+#channel = connection.channel()
+#channel.queue_declare(queue='task_queue', durable=True)
+#channel.basic_qos(prefetch_count=1)
+#channel.basic_consume(queue='task_queue', on_message_callback=callback)
+#channel.start_consuming()
