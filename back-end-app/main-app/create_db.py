abd7857d05a29b8f3e0a50df8a436142191369cd
pwatorski <piotr.watorski@gmail.com>
2021-11-18 20:16:33 +0100

    adding stock and api

--- back-end-app/main-app/create_db.py
+++ back-end-app/main-app/create_db.py
@@ -27,6 +27,17 @@ except:
     TQDM=False
     print("Consider instaling tqdm package for your Python interpreter:\npy -m pip install tqdm")
 
+def create_apisource(db, name:str):
+    apisource = Apisource(name)
+    db.session.add(apisource)
+    db.session.commit()
+    return apisource.id
+
+def create_stock(db, name:str, apisource_id:int):
+    stock = Stock(name, apisource_id)
+    db.session.add(stock)
+    db.session.commit()
+    return stock.id
 
 def drop_table(table_name):
     engine = create_engine(DATABASE_URI)
@@ -41,6 +52,10 @@ def clear_db(db):
 
     drop_table('action')
 
+    print('Creating apisource...')
+    api_id = create_apisource(db, 'Yahoo')
+    print('Creating stock...')
+    stock_id = create_stock(db, 'SomeStock', api_id)
 
 
     print('Creating Action, Company, Stock, Apisource tables...')
