040db106e20d930404e1ceda6fdbe4bd0194dd7b
pwatorski <piotr.watorski@gmail.com>
2021-11-26 00:44:11 +0100

    add prediction model

--- back-end-app/basicpredworker/app.py
+++ back-end-app/basicpredworker/app.py
@@ -2,12 +2,20 @@ import pika
 import psycopg2
 from datetime import datetime, timedelta
 import random
-
+import numpy
+import torch
+from sklearn.preprocessing import MinMaxScaler
+from model import LSTM
 try:
     db_conn = psycopg2.connect(database='sarna', user='postgres', host='postgress', password='sarna')
 except:
     db_conn = psycopg2.connect(database='sarna', user='postgres', host='0.0.0.0', password='sarna')
 cur = db_conn.cursor()
+
+
+model = LSTM()
+model.load_state_dict(torch.load('./base_model.model'))
+
 
 def run_model(steps:int=100, end:datetime=None, step_time:timedelta=None, beg_val:int=400, 
     stringify=False, fluctuation=100, as_tuples:bool=False):
@@ -28,6 +36,28 @@ def run_model(steps:int=100, end:datetime=None, step_time:timedelta=None, beg_va
     if as_tuples:
         return data
     return {t[0]: t[1] for t in data}
+
+def run_model2(raw_data, steps=20):
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    raw_data = numpy.array(raw_data)
+    fit_data = scaler.fit_transform(raw_data.reshape(-1,1))
+    fit_data = torch.from_numpy(fit_data).type(torch.Tensor)
+    fit_data = torch.reshape(fit_data, (1,-1, 1))
+    pred = fit_data.detach().clone()
+    for step in range(steps):
+        pred_step = model(pred)
+        pred_step = pred_step.detach()
+        pred_step = torch.reshape(pred_step, (1, 1, 1))
+        pred = torch.cat((pred, pred_step), dim=1)
+    pred = scaler.inverse_transform(torch.reshape(pred.detach(), (-1, 1)).numpy())
+    pred = list(pred.flatten()[-(steps+1):])
+    avg = sum(raw_data[-steps:]) / steps
+    noise = [x - avg for x in raw_data[-steps - 20:-20]]
+    noise[0]=0
+    noise[1]=0
+    pred = [x + y/2 for x, y in zip(pred, noise)]
+    return pred
+
 
 
 def callback(ch, method, properties, body):
@@ -60,22 +90,27 @@ def callback(ch, method, properties, body):
         cur.execute(delete_preds, (future_id,))
         db_conn.commit()
     print(f"\t[x] Clean future")
-    now_dt = datetime.now() + timedelta(days=20)
     steps = 20
-    newest_action_sql = "SELECT * FROM action WHERE company_id = %s ORDER BY timestamp DESC;"
+    newest_action_sql = "SELECT * FROM action WHERE company_id = %s ORDER BY timestamp DESC"
     cur.execute(newest_action_sql, (company_id,))
-    action_id, action_value, action_timestamp, action_company_id = cur.fetchone()
+    #action_id, action_value, action_timestamp, action_company_id = cur.fetchmany(100)
+    actions = cur.fetchmany(1000)
     print(f"\t[x] Predicting {steps} steps...")
-
-    pred = run_model(steps=steps, end=now_dt, step_time=timedelta(days=1), as_tuples=True, beg_val=action_value / 1000.0)
-
+    values = [x[1]/ 1000.0 for x in actions]
+    values.reverse()
+    #pred = run_model(steps=steps, end=now_dt, step_time=timedelta(days=1), as_tuples=True, beg_val=action_value / 1000.0)
+    pred = run_model2(values)
+    
     sql = "INSERT INTO prediction(value, timestamp, company_id) VALUES(%s,%s,%s)"
     print(f"\t[x] Inserting into future...")
-    cur.execute(sql, (action_value, action_timestamp, future_id, ))
-    db_conn.commit()
+    #cur.execute(sql, (action_value, action_timestamp, future_id, ))
+    #db_conn.commit()
+    start = datetime.fromtimestamp(actions[0][2])
+
     for r in pred:
-        cur.execute(sql, (int(r[1] * 1000), int(datetime.timestamp(r[0])), future_id))
+        cur.execute(sql, (int(r * 1000), int(datetime.timestamp(start)), future_id))
         db_conn.commit()
+        start += timedelta(days=1)
     print(f"\t[x] Future done!")
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
