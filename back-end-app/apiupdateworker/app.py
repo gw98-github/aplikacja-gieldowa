341a462e8b333fa5ff41a29235efe496b1e2a9b5
pwatorski <piotr.watorski@gmail.com>
2021-11-25 22:43:54 +0100

    add api update worker

--- back-end-app/apiupdateworker/app.py
+++ back-end-app/apiupdateworker/app.py
@@ -1,8 +1,9 @@
 import pika
+from sqlalchemy import sql
 import yfinance as yf
 import psycopg2
 from datetime import datetime
-
+import time
 
 
 
@@ -26,9 +27,36 @@ try:
 except:
     db_conn = psycopg2.connect(database='sarna', user='postgres', host='0.0.0.0', password='sarna')
 
-def run():
+cur = db_conn.cursor()
+
+find_company = "SELECT * FROM company ;"
+cur.execute(find_company)
+
+company = cur.fetchall()
+
+
+for company_id,c_name,c_symbol,c_stock in company:
+    sql = "SELECT * FROM action WHERE company_id = %s order by timestamp DESC;"
+    cur.execute(sql, (company_id,))
+    action = cur.fetchall()
+    print (action [0])
+    current_company = yf.Ticker(c_symbol)
+    values = current_company.history(period="1d", interval='1d')
+    print (values.index[0].to_pydatetime().strftime('%Y-%m-%dT%H:%M:00'))
     
-    pass
+    #List = []
+    #for i in values.index:
+        #List.append((i.to_pydatetime().strftime('%Y-%m-%dT%H:%M:00'),values.at[i,"Open"]))
 
-run()
 
+"""def run():
+
+    timeout = time.time() + 60*45
+
+    while True:
+
+        timeless = 0
+        if timeless == 45 or timeless > 45 or time.time() > timeout:
+    
+pass
+"""
