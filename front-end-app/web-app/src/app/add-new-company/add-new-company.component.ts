895382d387bdd7d30a095e6f69c2286f2e71c466
gw98-github <grzegorz.wazny.pv@gmail.com>
2021-11-27 17:19:53 +0100

    feat: progress bar

--- front-end-app/web-app/src/app/add-new-company/add-new-company.component.ts
+++ front-end-app/web-app/src/app/add-new-company/add-new-company.component.ts
@@ -10,7 +10,9 @@ export class AddNewCompanyComponent implements OnInit {
   candidates: any;
   filteredCandidates: any;
 
-  constructor(private stockDataService: StockDataService) {}
+  constructor(private stockDataService: StockDataService) {
+    console.log(this.filteredCandidates);
+  }
 
   ngOnInit(): void {
     this.stockDataService.getCandidatesList().subscribe((response) => {
