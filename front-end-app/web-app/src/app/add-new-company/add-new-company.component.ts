973dba8f2cfe738915e51ed35849ad4460ac8929
gw98-github <grzegorz.wazny.pv@gmail.com>
2021-11-27 16:05:10 +0100

    feat: search+filter

--- front-end-app/web-app/src/app/add-new-company/add-new-company.component.ts
+++ front-end-app/web-app/src/app/add-new-company/add-new-company.component.ts
@@ -8,12 +8,27 @@ import { StockDataService } from '../services/stock-data.service';
 })
 export class AddNewCompanyComponent implements OnInit {
   candidates: any;
+  filteredCandidates: any;
 
   constructor(private stockDataService: StockDataService) {}
 
   ngOnInit(): void {
     this.stockDataService.getCandidatesList().subscribe((response) => {
       this.candidates = response.candidates;
+      this.filteredCandidates = this.candidates;
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value
+      .trim()
+      .toLowerCase();
+
+    this.filteredCandidates = this.candidates.filter((s: any) => {
+      return (
+        s[0].trim().toLowerCase().includes(filterValue) ||
+        s[1].trim().toLowerCase().includes(filterValue)
+      );
     });
   }
 }
