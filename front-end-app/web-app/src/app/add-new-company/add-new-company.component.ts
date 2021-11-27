64364b2df40c9ab6aa99d8c96d228b0af436f89b
gw98-github <grzegorz.wazny.pv@gmail.com>
2021-11-27 15:32:22 +0100

    feat: companies list display

--- front-end-app/web-app/src/app/add-new-company/add-new-company.component.ts
+++ front-end-app/web-app/src/app/add-new-company/add-new-company.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { StockDataService } from '../services/stock-data.service';
 
 @Component({
   selector: 'app-add-new-company',
   templateUrl: './add-new-company.component.html',
-  styleUrls: ['./add-new-company.component.css']
+  styleUrls: ['./add-new-company.component.css'],
 })
 export class AddNewCompanyComponent implements OnInit {
+  candidates: any;
 
-  constructor() { }
+  constructor(private stockDataService: StockDataService) {}
 
   ngOnInit(): void {
+    this.stockDataService.getCandidatesList().subscribe((response) => {
+      this.candidates = response.candidates;
+    });
   }
-
 }
